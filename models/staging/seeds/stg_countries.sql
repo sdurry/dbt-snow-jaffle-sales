ac49844bcb5a3db978fdca3204456d3fea825359
Stephan Durry <stephan.durry@fishtownanalytics.com>
2025-05-09 13:51:04 +0000

    add my new model

---
+++ models/staging/seeds/stg_countries.sql
@@ -0,0 +1,13 @@
+with countries as (
+
+    select *
+    from {{ ref('country') }}
+
+), renamed as (
+
+    select
+        country as country_code,
+        name as country_name
+    from countries
+
+)
